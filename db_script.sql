a5d07066a8ce3fd671ae23e4f072787abe846b9d
alex-vlado-yoanski <94299195+alex-vlado-yoanski@users.noreply.github.com>
2021-12-29 19:46:27 +0200

    Update db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -11,8 +11,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
     id        	INT GENERATED ALWAYS AS IDENTITY,
-    name       	VARCHAR(50) NOT NULL,
-	password	VARCHAR(97) NOT NULL,
+    name       	VARCHAR(50) 	NOT NULL,
+	password	VARCHAR(97) 	NOT NULL,
 	
 	CONSTRAINT pk_users PRIMARY KEY(id)
 );
@@ -21,10 +21,10 @@ CREATE TABLE users(
 
 CREATE TABLE user_invitations(
     id        		INT GENERATED ALWAYS AS IDENTITY,
-    status       	INT NOT NULL,
-	sender_id		INT NOT NULL,
-	receiver_id		INT NOT NULL,
-	create_date		TIMESTAMP NOT NULL,
+    status       	INT 		NOT NULL,
+	sender_id		INT 		NOT NULL,
+	receiver_id		INT 		NOT NULL,
+	create_date		TIMESTAMP 	NOT NULL,
 	
 	CONSTRAINT pk_user_invitations PRIMARY KEY(id),
 	CONSTRAINT fk_user_invitations_sender_id FOREIGN KEY(sender_id) REFERENCES users(id),
@@ -35,10 +35,10 @@ CREATE TABLE user_invitations(
 
 CREATE TABLE messages(
     id        		INT GENERATED ALWAYS AS IDENTITY,
-    text     		VARCHAR(256) NOT NULL,
-	file_path		VARCHAR(260) NOT NULL,
-	create_date		TIMESTAMP NOT NULL,
-	sent_by			INT NOT NULL,
+    text     		VARCHAR(256) 	NOT NULL,
+	file_path		VARCHAR(260) 	NOT NULL,
+	create_date		TIMESTAMP 		NOT NULL,
+	sent_by			INT 			NOT NULL,
 	
 	CONSTRAINT pk_messages PRIMARY KEY(id),
 	CONSTRAINT fk_messages_sent_by FOREIGN KEY(sent_by) REFERENCES users(id)
