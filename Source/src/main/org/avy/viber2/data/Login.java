e8c2aa3cea6f3742049e6cd809c4c4bb4a65bf50
alex-vlado-yoanski <alex-vlado-yoanski@abv.bg>
2021-12-29 19:27:52 +0200

    Модофициран Login да ползва servlet-и

--- Source/src/main/org/avy/viber2/data/Login.java
+++ Source/src/main/org/avy/viber2/data/Login.java
@@ -1,50 +1,29 @@
 package org.avy.viber2.data;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
-import org.avy.viber2.database.DatabaseConnection;
-import org.avy.viber2.tables.mapping.User;
-import org.hibernate.Session;
+@WebServlet(name = "LoginServlette", urlPatterns = "/login")
+public class Login extends HttpServlet {
+    private static final long serialVersionUID = 4970223239827455229L;
 
-import com.google.gson.Gson;
-
-public class Login {
-    static boolean IsUserAuthenticationCorrect(Object FromJSON, Session session) {
-	// Изчитане на потребителите от базата данни
-	CriteriaBuilder builder = session.getCriteriaBuilder();
-	CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
-	criteriaQuery.from(User.class);
-
-	List<User> users = session.createQuery(criteriaQuery).getResultList();
-
-	// Сравняване на 
-	/*for (User user : users) {
-	    if (user.getName().equals(((User) FromJSON).getName())) {
-		if (u.getPassword().equals(((User) FromJSON).getPassword())) {
-		    return true;
-		}
-	    }
-	}
-*/
-	return true;
+    @Override
+    public void init() throws ServletException {
+	// Не се ползва
     }
 
-    static Object StringToObject(StringBuffer dataString) {
-	System.out.println(dataString); // За дебъгване премахни
-	Gson gson = new Gson();
-	User user2 = gson.fromJson(dataString.toString(), User.class); // JSON в клас
-
-	return user2;
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	String JsonFromClient = request.getParameter("help_me");
     }
 
+    @Override
+    public void destroy() {
+	// Не се ползва
+    }
 }
