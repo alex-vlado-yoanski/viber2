02958edb1a2359502fd57b3a9772796d67907f8d
alex-vlado-yoanski <alex-vlado-yoanski@abv.bg>
2021-12-30 21:34:10 +0200

    test socket пореден

--- Source/src/main/org/avy/viber2/RequestHandler.java
+++ Source/src/main/org/avy/viber2/RequestHandler.java
@@ -10,6 +10,7 @@ import org.avy.viber2.tables.mapping.User;
 import com.google.gson.*;
 
 public class RequestHandler extends Thread {
+    private static final int messageLength = 1500;
     private Socket socket;
 
     public RequestHandler(Socket socket) {
@@ -21,20 +22,13 @@ public class RequestHandler extends Thread {
 	try {
 	    // Прочитане на заявката
 	    System.out.println("Reading request...");
-
-	    // InputStreamReader inputStream = new
-	    // InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
-	    // BufferedReader dataIn = new BufferedReader(inputStream);
-
-	    byte line_bytes[] = new byte[1500];
-	    socket.getInputStream().read(line_bytes);
-
-	    String request = new String(line_bytes, StandardCharsets.UTF_8);
-	    request = request.trim();
-	    /*
-	     * String line = dataIn.readLine(); while (line != null && line.length() > 0) {
-	     * request += line; line = dataIn.readLine(); }
-	     */
+	    byte[] inputMessage = new byte[messageLength];
+	    
+	    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+	    inputStream.read(inputMessage, 0, messageLength);
+	    
+	    String request = new String(inputMessage, StandardCharsets.UTF_8);
+	    request = request.trim(); // за всеки случай
 	    System.out.println(request);
 
 	    // Обработка на заявката
@@ -42,19 +36,17 @@ public class RequestHandler extends Thread {
 
 	    String response = process(request);
 	    System.out.println(response);
-
+	    
 	    // Изпращане на отговор на заявката
 	    System.out.println("Returning response...");
-	    PrintWriter dataOut = new PrintWriter(socket.getOutputStream(), true);
-
-	    dataOut.print(response);
-	    dataOut.flush();
+	    PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
+	    outputStream.print(response);
 
 	    // Затваряне на връзката
-	    // dataIn.close();
-	    dataOut.close();
+	    System.out.println("Closing response...");
+	    inputStream.close();
+	    outputStream.close();
 	    socket.close();
-	    System.out.println("Done\n\n\n");
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
@@ -64,12 +56,11 @@ public class RequestHandler extends Thread {
 	String response = null;
 
 	try {
-	    Gson gson = new GsonBuilder().registerTypeAdapter(User.class, new LoginDataHandler()).setPrettyPrinting()
-		    .create();
-	    User user = gson.fromJson(request, User.class);
-
-	    response = gson.toJson(user);
-	    // response = "test44";
+	    //Gson gson = new GsonBuilder().registerTypeAdapter(User.class, new LoginDataHandler()).setPrettyPrinting().create();
+	    //User user = gson.fromJson(request, User.class);
+	    //response = gson.toJson(user);
+	    
+	    response = "test44";
 	} catch (Exception e) {
 	    System.out.println("Processing request failed.");
 	    e.printStackTrace();
