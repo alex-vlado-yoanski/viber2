45e8d8b2c0a7969df6be97f163969e177d1ad0c7
alex-vlado-yoanski <alex-vlado-yoanski@abv.bg>
2021-12-30 18:56:25 +0200

    Комуникация осъществена чрез масив от байтове, в UTF-8 кодировка

--- Source/src/main/org/avy/viber2/Main.java
+++ Source/src/main/org/avy/viber2/Main.java
@@ -2,7 +2,7 @@ package org.avy.viber2;
 
 public class Main {
     public static void main(String[] args) {	
-	Server server = new Server(8080, 360*1000);
+	Server server = new Server(8080, 60*1000);
 	server.start();
     }
 }
--- Source/src/main/org/avy/viber2/RequestHandler.java
+++ Source/src/main/org/avy/viber2/RequestHandler.java
@@ -2,74 +2,80 @@ package org.avy.viber2;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 import org.avy.viber2.data.*;
+import org.avy.viber2.tables.mapping.User;
+
 import com.google.gson.*;
 
 public class RequestHandler extends Thread {
     private Socket socket;
-    
+
     public RequestHandler(Socket socket) {
 	this.socket = socket;
     }
-    
+
     @Override
-    public void run()
-    {
+    public void run() {
 	try {
 	    // Прочитане на заявката
 	    System.out.println("Reading request...");
-	    
-	    InputStreamReader inputStream = new InputStreamReader(socket.getInputStream());
-	    BufferedReader dataIn = new BufferedReader(inputStream);
 
-	    String request = null;
-	    
-	    String line = dataIn.readLine();
-	    while(line != null && line.length() > 0)
-	    {
-		request += line;
-		line = dataIn.readLine();
-	    }
-	    
+	    // InputStreamReader inputStream = new
+	    // InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+	    // BufferedReader dataIn = new BufferedReader(inputStream);
+
+	    byte line_bytes[] = new byte[1500];
+	    socket.getInputStream().read(line_bytes);
+
+	    String request = new String(line_bytes, StandardCharsets.UTF_8);
+	    request = request.trim();
+	    /*
+	     * String line = dataIn.readLine(); while (line != null && line.length() > 0) {
+	     * request += line; line = dataIn.readLine(); }
+	     */
 	    System.out.println(request);
-	    
+
 	    // Обработка на заявката
 	    System.out.println("Procesing request...");
-	    
+
 	    String response = process(request);
 	    System.out.println(response);
-	    
-	    // Изпращане на отговор на заявката	    
+
+	    // Изпращане на отговор на заявката
 	    System.out.println("Returning response...");
 	    PrintWriter dataOut = new PrintWriter(socket.getOutputStream(), true);
-	    
-	    dataOut.println(response);
-            dataOut.flush();
-            
-            // Затваряне на връзката
-            dataIn.close();
-            dataOut.close();
+
+	    dataOut.print(response);
+	    dataOut.flush();
+
+	    // Затваряне на връзката
+	    // dataIn.close();
+	    dataOut.close();
 	    socket.close();
+	    System.out.println("Done\n\n\n");
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
     }
-    
+
     private String process(String request) {
 	String response = null;
-	
+
 	try {
-	    //Gson gson = new GsonBuilder().registerTypeAdapter(User.class, new LoginDataHandler()).setPrettyPrinting().create();
-	    //User user = gson.fromJson(request, User.class);
-	    
-	    //response = gson.toJson(user);
-	    response = "test44";
+	    Gson gson = new GsonBuilder().registerTypeAdapter(User.class, new LoginDataHandler()).setPrettyPrinting()
+		    .create();
+	    User user = gson.fromJson(request, User.class);
+
+	    response = gson.toJson(user);
+	    // response = "test44";
 	} catch (Exception e) {
 	    System.out.println("Processing request failed.");
 	    e.printStackTrace();
 	}
-	
+
 	return response;
     }
-    
+
 }
